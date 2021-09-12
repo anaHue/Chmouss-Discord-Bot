703e04e369705c82020726ff158b5377829537d0
HUET Anatole <anatole.huet@student.vinci.be>
2021-09-12 20:35:14 +0200

    modify - change announcement message

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ async function checkIfChmoussIsInLive(){
         const annoucementChannel = client.channels.cache.get(process.env.ANNOUNCEMENT_CHANNEL_ID);
         annoucementChannel.send("Hey " + client.guilds.cache.toJSON()[0].roles.everyone.toString() + "!\n\n"
                             + "Chmouss is live on Twitch, now playing **" + result.data[0].game_name + "** !\n\n"
-                            + result.data[0].title
+                            + "**" + result.data[0].title + "**"
                             + "Be sure to check it out :blue_heart: https://www.twitch.tv/chmouss");
         inLive = true;
 
